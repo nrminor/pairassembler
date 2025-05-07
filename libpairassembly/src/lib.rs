f02c853c689fb34c48cc7f66cd9411498533c54b
Nicholas Minor <nicholasrminor@gmail.com>
2025-05-07 10:15:48 -0500

    refactor to start unpacking bbmerge implementation

--- libpairassembly/src/lib.rs
+++ libpairassembly/src/lib.rs
@@ -1,4 +1,11 @@
-pub mod overlap;
+pub mod overlap {
+
+    pub mod methods {
+        pub mod bbmerge;
+        pub mod fastp;
+        pub mod vsearch;
+    }
+}
 pub mod prelude;
 
 // re-exports
